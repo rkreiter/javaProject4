bdc96f7167f2fc52f02f2503986fdc2e77e6a846
rkreiter <ryankreiter@yahoo.com>
2013-11-21 19:01:59 -0500

    Client working with asher

--- src/server/StartClient.java
+++ src/server/StartClient.java
@@ -16,6 +16,7 @@ public class StartClient{
    		//This will make a pop up for player in gui
    	    //Send number of players to server
 		case '1':
+	    out.println("Sending Num Players");
     	    c.sendString("1", 0);
     	    String recvdStr = c.getResponse();
     	    interpretResponse(recvdStr, c, b, p);
@@ -46,7 +47,7 @@ public class StartClient{
 	    String recvdStr;
 	    
 	    //Create Client
-	    theClient = new ClientServerSocket("192.168.1.250", 4000);
+	    theClient = new ClientServerSocket("67.194.56.42", 4000);
 	    theClient.startClient();
 	    
 	    //Get response from server
@@ -54,6 +55,7 @@ public class StartClient{
 	    interpretResponse(recvdStr, theClient, board, player);  
 	    
 	    //Send Init Player request and wait for response
+	    out.println("Sending player name");
 	    theClient.sendName("ASHER");
 	    recvdStr = theClient.getResponse();
 	    interpretResponse(recvdStr, theClient, board, player);
