6f7950d11fa72aa6955b2209fee487bc21375f28
nemetro <nemetro@umich.edu>
2013-11-21 22:02:18 -0500

    Fixed button size

--- src/intro/startFrame.java
+++ src/intro/startFrame.java
@@ -1,21 +1,20 @@
 package intro;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
+import static java.lang.System.out;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.applet.*;
+import javax.imageio.*;
+import java.io.*;
+import java.util.*;
+import java.net.*;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
-public class startFrame extends JFrame{
-	
+public class startFrame extends JFrame
+{
   JPanel mainScreen = new JPanel(new BorderLayout());
 
   JButton play;
@@ -26,22 +25,15 @@ public class startFrame extends JFrame{
 
   JLabel title;
   JPanel buttons = new JPanel(new GridLayout(5,1));
+  JPanel buttons2 = new JPanel(new FlowLayout());
   
-  final static Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-  final static int INTRO_WIDTH = (int) SCREEN_SIZE.getWidth();
-  final static int INTRO_HEIGHT = (int) SCREEN_SIZE.getHeight();
-  
-  public startFrame(String intit){
-	  
+ 
+  public startFrame(String intit)
+  {
     //set title
     super(intit);
     setLayout(new BorderLayout());
-    ImageIcon icon = new ImageIcon("src/images/introbackground.png"); 
-    Image img = icon.getImage();  
-    Image newimg = img.getScaledInstance(INTRO_WIDTH, INTRO_HEIGHT,
-    										java.awt.Image.SCALE_SMOOTH);  
-    icon = new ImageIcon(newimg);
-    setContentPane(new JLabel(icon));
+    setContentPane(new JLabel(new ImageIcon("images/introbackground.png")));
     setLayout(new BorderLayout());
     getContentPane().setBackground(Color.BLACK);
 
@@ -50,6 +42,7 @@ public class startFrame extends JFrame{
     instr = new JButton("How To Play");
     tutor = new JButton("Tutorial");
     about = new JButton("About");
+
 
     JLabel fill = new JLabel("    ");
     fill.setBackground(Color.BLACK);
@@ -60,15 +53,11 @@ public class startFrame extends JFrame{
     buttons.add(about);
     buttons.setOpaque(false);
 
+    buttons2.add(buttons);
+    buttons2.setOpaque(false);
     
     add(title, BorderLayout.NORTH);
     add(fill, BorderLayout.CENTER);
-<<<<<<< HEAD
-    add(buttons, BorderLayout.SOUTH);
-=======
-    add(buttons, BorderLayout.EAST);
-
-    pack();  
->>>>>>> 1495540f8d70d140dc8f9e521c80eea23944cda3
+    add(buttons2, BorderLayout.SOUTH);
   }
 }
